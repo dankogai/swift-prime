20471220536fdb0f20630cea9454e255ad908f47
Dan Kogai <dankogai+github@gmail.com>
2014-08-06 13:54:30 +0900

    retouch examples

--- prime/main.swift
+++ prime/main.swift
@@ -6,9 +6,10 @@
 //  Copyright (c) 2014 Dan Kogai. All rights reserved.
 //
 
-
+println(2.isPrime)
+println(42.isPrime)
+println(0x7FFFffff.isPrime)
 println(0.nextPrime)
-println(9223372036854775783.prevPrime)
 println(Int.max.prevPrime)
 for p in Int.Prime() {
     if p > 10 { break }

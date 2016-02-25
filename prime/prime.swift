d84193e9f3631533c083aa24d512d0c2719a92db
Tiza59 <uwefalck@mac.com>
2016-02-25 12:29:51 +0100

    Remove superfluous braces

--- prime/prime.swift
+++ prime/prime.swift
@@ -73,7 +73,7 @@ public extension UInt {
     }
     /// Integer Square Root of `n`
     public static func isqrt(n:UInt)->UInt {
-        var xk = UInt((sqrt(Double(n))))
+        var xk = UInt(sqrt(Double(n)))
         if xk <= 67108864 { return (xk)}  //sqrt(2^52)
         repeat {
             let xk1 = (xk + n / xk) / 2
